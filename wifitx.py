774cc2cf0d90e3afe12a0c7a151a1e27b9393c8d
Nick Everett <njeverett@gmail.com>
2018-01-08 16:56:34 +0000

    Wifi Tx
    Updated handle Keyboard Interupts

--- wifitx.py
+++ wifitx.py
@@ -4,31 +4,35 @@
 
 import os
 import re
+import sys
 import time
 
 
-def return_numbers(string):   # Returns only numbers from an input string
-    values = re.findall("\d",string)
+def return_numbers(string):  # Returns only numbers from an input string
+    values = re.findall("\d", string)
     return "".join(values)
 
 
-def time_now():     # Returns time when run
+def time_now():  # Returns time when run
     time_now = time.strftime("%H:%M:%S", time.gmtime())
     return time_now
 
 
-def get_SSID():     # Returns SSID of the connected wifi network from osx
-    SSID = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep ' SSID'").readline()
+def get_SSID():  # Returns SSID of the connected wifi network from osx
+    SSID = os.popen(
+        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep ' SSID'").readline()
     SSID = SSID.strip(" \n")
     return SSID
 
-def get_channel():      # Returns the channel info of the connected wifi network from osx
-    channel = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep channel").readline()
+
+def get_channel():  # Returns the channel info of the connected wifi network from osx
+    channel = os.popen(
+        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep channel").readline()
     channel = channel.strip(" \n")
     return channel
 
 
-def calc_freq(channel):     # Determines if 2.4g of 5g network. Requires int input
+def calc_freq(channel):  # Determines if 2.4g of 5g network. Requires int input
     if 1 <= channel <= 11:
         return "2.4Ghz"
     elif 3000 <= channel <= 8800:
@@ -38,20 +42,27 @@ def calc_freq(channel):     # Determines if 2.4g of 5g network. Requires int inp
 
 
 def measure_tx():  # Returns wifi tx rate from osX
-    tx = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep lastTxRate").readline()
+    tx = os.popen(
+        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep lastTxRate").readline()
     return (tx)
 
-"""Start up messages"""
-print("Starting Wifi TX Tracker\n(Use cntl +c to end)\n")
-channel_number = get_channel()
-channel_number = int(return_numbers(channel_number))
-print("{}\n{} - {}\n".format(get_SSID(), get_channel(), calc_freq(channel_number)))
-time.sleep(1)
 
+try:
+    """Start up messages"""
+    print("Starting Wifi TX Tracker\n(Use cntl +c to end)\n")
+    channel_number = get_channel()
+    channel_number = int(return_numbers(channel_number))
+    print("{}\n{} - {}\n".format(get_SSID(), get_channel(), calc_freq(channel_number)))
+    time.sleep(1)
+except (KeyboardInterrupt, SystemExit):
+    sys.exit("\nKeyboard Interrupt received, exiting...")
 
 while True:
-    measure_value = measure_tx()
-    measure_value = return_numbers(measure_value)
-    measure_time = time_now()
-    print("{} - {} Mbps".format(measure_time, measure_value))
-    time.sleep(2)
+    try:
+        measure_value = measure_tx()
+        measure_value = return_numbers(measure_value)
+        measure_time = time_now()
+        print("{} - {} Mbps".format(measure_time, measure_value))
+        time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit("\nKeyboard Interrupt received, exiting...")
